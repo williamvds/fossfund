297c13c23333a35db2c7b9c82e684983fe8cdfd6
williamvds <w.vigolodasilva@gmail.com>
2018-08-11 13:33:42 +0100

    gunicorn: reload, formatting

--- gunicorn.py
+++ gunicorn.py
@@ -5,4 +5,5 @@ bind = 'localhost:8080'
 proxy_allow_from = 'localhost,127.0.0.1'
 forwarded_allow_ips = 'localhost,127.0.0.1'
 workers = multiprocessing.cpu_count()
-worker_class='aiohttp.worker.GunicornWebWorker'
+worker_class = 'aiohttp.worker.GunicornWebWorker'
+reload = True
