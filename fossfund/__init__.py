43fedac325576a41ec7274dad1cd175a34d41b6f
williamvds <w.vigolodasilva@gmail.com>
2018-08-11 23:26:53 +0100

    Simplify main file

--- fossfund/__init__.py
+++ fossfund/__init__.py
@@ -1,5 +1,5 @@
 """Functions for running the application"""
-import sys, os
+import sys, os, asyncio
 from base64 import urlsafe_b64decode
 
 import yaml
@@ -13,48 +13,34 @@ from attrdict import AttrDict
 from . import db, routes
 from .extends import handleError, attachUser
 
-configFile = os.path.join(os.path.dirname(__file__), '../config.yaml')
-def run(conf=configFile):
-    """Start server and setup"""
+_configFile = os.path.join(os.path.dirname(__file__), '../config.yaml')
+app = None
 
-    global app
-    app = web.Application(middlewares=[handleError])
-    app.config = loadConfig(conf)
+try:
+    _config = AttrDict(yaml.load(open(_configFile)))
+except IOError:
+    print('Execption: %s\nFailed to load configuration file %s' \
+        % (sys.exc_info()[0], _configFile))
+    sys.exit(1)
 
-    routes.addRoutes(app.router)
-
-    jinjaSetup(app, loader=jinjaLoader(os.path.dirname(__file__)),
-        context_processors=[request_processor],
-        **app.config.jinja)
-
-    sessionSetup(app, EncryptedCookieStorage(
-        urlsafe_b64decode(app.config.sessionSecret),
-        cookie_name='session'))
-
-    app.middlewares.append(attachUser)
-    app.on_startup.append(db.attach)
-    app.on_shutdown.append(db.destroy)
-
-def setup(conf=configFile):
-    """Rebuild database tables"""
-    import asyncio
+if sys.argv[1] == 'setup':
     asyncio.get_event_loop().run_until_complete(
-        db.setup(loadConfig(conf).db))
+        db.setup(_config.db))
+    sys.exit(0)
 
+app = web.Application(middlewares=[handleError])
+app.config = _config
 
-def loadConfig(conf):
-    """Load the config from given file or default"""
-    try:
-        return AttrDict(yaml.load(open(conf)))
-    except IOError:
-        print('Execption: %s\nFailed to load conf file %s' % (sys.exc_info()[0], conf))
-        sys.exit(1)
+routes.addRoutes(app.router)
 
-if __name__ == '__main__':
-    try:
-        method = locals()[sys.argv[1]]
-    except (NameError, IndexError):
-        method = run
-    method()
-else:
-    run()
+jinjaSetup(app, loader=jinjaLoader(os.path.dirname(__file__)),
+    context_processors=[request_processor],
+    **app.config.jinja)
+
+sessionSetup(app, EncryptedCookieStorage(
+    urlsafe_b64decode(app.config.sessionSecret),
+    cookie_name='session'))
+
+app.middlewares.append(attachUser)
+app.on_startup.append(db.attach)
+app.on_shutdown.append(db.destroy)
